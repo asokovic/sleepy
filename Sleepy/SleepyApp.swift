a68e24653ce7fe7d1f2c1c8ab1d0165fc4c20b32
Andrija Sokovic <sokovic@Andrijas-MacBook-Pro-2.local>
2025-10-29 13:55:12 +0100

    Initial Xcode project

---
+++ Sleepy/SleepyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepyApp.swift
+//  Sleepy
+//
+//  Created by Andrija Sokovic on 29. 10. 2025..
+//
+
+import SwiftUI
+
+@main
+struct SleepyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
